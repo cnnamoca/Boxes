9513225142d4a40aec4be7b423b8e1884c4a3ef8
Carlo <carlo.namoca@gmail.com>
2017-10-03 11:23:20 -0700

    Add Box class and three new properties

---
+++ Boxes/Box.h
@@ -0,0 +1,20 @@
+//
+//  Box.h
+//  Boxes
+//
+//  Created by Carlo Namoca on 2017-10-03.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Box : NSObject
+//set properties
+{
+    float height;
+    float width;
+    float length;
+}
+
+
+@end
---
+++ Boxes/Box.m
@@ -0,0 +1,13 @@
+//
+//  Box.m
+//  Boxes
+//
+//  Created by Carlo Namoca on 2017-10-03.
+//  Copyright © 2017 Carlo Namoca. All rights reserved.
+//
+
+#import "Box.h"
+
+@implementation Box
+
+@end
